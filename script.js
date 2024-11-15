a09303968b5776e7e6d62bfd9b1978cdae4797f7
Segun David <segdavid03@gmail.com>
2024-11-15 16:39:23 +0100

    feat: implemented the click show accordion content functionality

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 "use script";
 
-console.log("setup project folders");
+// getting selectors
+const accordionMain = document.querySelectorAll(".accordion--main");
+const accordionPanel = document.querySelectorAll(".accordion--panel");
+// console.log(accordionMain);
+
+// looping through each nodeList
+for (let i = 0; i < accordionMain.length; i++) {
+  accordionMain[i].addEventListener("click", function () {
+    // selecting each nodeList
+    accordionPanel[i].classList.toggle("hidden");
+    // toggle each panel by dynamically adding and removing the hidden class
+
+    // change svg image
+  });
+}
